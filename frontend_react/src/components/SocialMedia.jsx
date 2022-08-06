3d38f32df36fc24b7cb82349bce97523b8b2eead
ivsumitkumar <vishnusumit2@gmail.com>
2022-08-06 16:43:56 +0530

    added faviicon

--- frontend_react/src/components/SocialMedia.jsx
+++ frontend_react/src/components/SocialMedia.jsx
@@ -1,18 +1,21 @@
 import React from 'react'
-import {BsTwitter, BsInstagram} from 'react-icons/bs'
-import {FaFacebookF} from 'react-icons/fa'
+import {BsTwitter, BsInstagram, BsGithub,BsLinkedin} from 'react-icons/bs'
+// import {FaFacebookF} from 'react-icons/fa'
 
 const SocialMedia = () => {
   return (
     <div className='app__social'>
         <div>
-            <BsTwitter/>
+        <a href="https://www.linkedin.com/in/sumit-kumar-77056a121" target="_blank" alt="Linkedin"><BsLinkedin/></a>
         </div>
         <div>
-            <FaFacebookF/>
+        <a href="https://www.github.com/ivsumitkumar" target="_blank" alt="FaceBook"><BsGithub/></a>
         </div>
         <div>
-            <BsInstagram/>
+            <a href="https://twitter.com/its_sumitkumar" target="_blank" alt="twitter"><BsTwitter/></a>
+        </div>
+        <div>
+            <a href="https://www.instagram.com/sumitkumar.in/" target="_blank" alt="Instagram"><BsInstagram/></a>
         </div>
     </div>
   )
